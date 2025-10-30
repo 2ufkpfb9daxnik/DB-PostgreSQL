b18296850242928231a7ee743e9536838c58f359
2ufkpfb9daxnik <115055404+2ufkpfb9daxnik@users.noreply.github.com>
2025-10-30 12:22:50 +0900

    5-5tx

---
+++ sql/05/drill/ex-01_2.sql
@@ -0,0 +1,20 @@
+START TRANSACTION;
+
+SELECT
+    last_login_at,
+    created_on
+FROM
+    s_characters;
+
+DELETE FROM s_characters
+WHERE
+    (last_login_at IS NULL) AND
+    (created_on <= '2023-12-31');
+
+SELECT
+    last_login_at,
+    created_on
+FROM
+    s_characters;
+
+ROLLBACK;
